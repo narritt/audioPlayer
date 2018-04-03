274ebec86d347dd35c7e3ffe4f00061d215313e0
narritt <narritt@yandex.ru>
2018-04-03 17:15:34 +0300

    Playlist and AddSong adapters and new layout for song in add-to-playlist-menu

---
+++ app/src/main/java/com/example/narritt/audioplayer/adapters/AddSongAdapter.java
@@ -0,0 +1,61 @@
+package com.example.narritt.audioplayer.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.narritt.audioplayer.R;
+import com.example.narritt.audioplayer.items.Song;
+
+import java.util.ArrayList;
+
+
+public class AddSongAdapter extends BaseAdapter {
+
+    private ArrayList<Song> songs;
+    private LayoutInflater songInf;
+
+    public AddSongAdapter(Context c, ArrayList<Song> theSongs){
+        songs = theSongs;
+        songInf = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //map to song layout
+        LinearLayout songLay = (LinearLayout)songInf.inflate(R.layout.add_song, parent, false);
+        //get title and artist views
+        TextView songView   = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        //get song using position
+        Song currSong = songs.get(position);
+        //get title and artist strings
+        songView.setText(currSong.getTitle());
+        artistView.setText(currSong.getArtist());
+        //set position as tag
+        songLay.setTag(position);
+        return songLay;
+    }
+
+}
---
+++ app/src/main/java/com/example/narritt/audioplayer/adapters/PlaylistAdapter.java
@@ -0,0 +1,68 @@
+package com.example.narritt.audioplayer.adapters;
+
+/**
+ * Created by Narritt on 23.03.2018.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.narritt.audioplayer.R;
+
+import java.util.ArrayList;
+
+import com.example.narritt.audioplayer.items.Playlist;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Narritt on 13.01.2018.
+ */
+
+public class PlaylistAdapter extends BaseAdapter {
+
+    private ArrayList<Playlist> playlists;
+    private LayoutInflater artistInf;
+
+    public PlaylistAdapter(Context c, ArrayList<Playlist> thePlaylists){
+        playlists = thePlaylists;
+        artistInf = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return playlists.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //map to artist layout
+        LinearLayout artistLay = (LinearLayout)artistInf.inflate(R.layout.artist, parent, false);
+
+        TextView artistNameView   = (TextView)artistLay.findViewById(R.id.artistName);
+
+        Playlist currPlaylist = playlists.get(position);
+
+        artistNameView.setText(currPlaylist.getName());
+
+        artistLay.setTag(position);
+        return artistLay;
+    }
+}
